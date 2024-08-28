1c39b9d0dbe2e4ebb1f8055ef66e303737607822
nebilesamelsuleymna <nebilesmaelprog@gmail.com>
2024-08-28 05:56:15 -0700

    adminlte template adjustment

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('admin-page',function(){
+    return view('admin.layouts.app');
+});
+
 require __DIR__.'/auth.php';
